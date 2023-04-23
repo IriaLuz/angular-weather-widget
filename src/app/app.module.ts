6a679089cdfa4f0a677fc017ba7f7888a1763488
Iria Luz Suarez Alvarado <irialuz@C02DR4AFMD6M.local>
2023-04-23 16:11:17 +0100

    feature: create city weather component with hardcode data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CityWeatherComponent } from './components/city-weather/city-weather.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CityWeatherComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
